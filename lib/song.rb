fb1fc84a67d6b99eaf8dd5c37bec8af4e68c28fb
David Gomara <davidgomara7@gmail.com>
2020-11-21 00:27:26 +0000

    commit tree 5bc98dd71f7ead7ce92034ea7b4021ab083444b3

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,67 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end 
+  
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end 
+  
+  def self.find_by_name(name)
+    self.all.detect{ |song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+    #What I did worked but learn isnt validating. It is saying I'm not using .find_by_name
+    #Learns answer is nicer looking either way
+    #
+    #if Song.all.collect{ |song| song.name == name}.include?(true)
+      #Song.find_by_name(name)
+    #else 
+      #self.create_by_name(name)
+    #end 
+  end 
+  
+  def self.alphabetical
+    self.all.collect{ |song| song.name}.sort.collect{ |name| Song.find_by_name(name)}
+  end 
+  
+  def self.new_from_filename(file)
+    song = Song.new
+    name = file.split(/[.-]/)[1].strip
+    song.name = name
+    artist_name = file.split(/[.-]/)[0].strip
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.create_from_filename(file)
+    song = Song.new
+    name = file.split(/[.-]/)[1].strip
+    song.name = name
+    artist_name = file.split(/[.-]/)[0].strip
+    song.artist_name = artist_name
+    @@all << song
+    song
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
 
 end
